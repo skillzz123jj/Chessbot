d84d728a3d9804de1b4d2a7d4a42a9f1671e224f
Mila <turpeinenmila6@gmail.com>
2026-01-24 03:28:16 +0200

    Add movement logic for Knight

--- shakki/nappula.cpp
+++ shakki/nappula.cpp
@@ -21,7 +21,46 @@ void Torni::annaSiirrot(std::list<Siirto>& lista, Ruutu* ruutu, Asema* asema, in
 
 void Ratsu::annaSiirrot(std::list<Siirto>& lista, Ruutu* ruutu, Asema* asema, int vari)
 {
-	
+	int x = ruutu->getRivi();
+	int y = ruutu->getSarake();
+
+	int siirrot[8][2] = {
+	   { 2,  1}, { 2, -1},
+	   {-2,  1}, {-2, -1},
+	   { 1,  2}, { 1, -2},
+	   {-1,  2}, {-1, -2}
+	};
+
+	for (int i = 0; i < 8; ++i)
+	{
+		int tempX = x + siirrot[i][0];
+		int tempY = y + siirrot[i][1];
+
+		//Katsotaan meneekˆ laudasta ulos
+		if (tempX < 0 || tempX >= 8 || tempY < 0 || tempY >= 8)
+			continue;
+
+		if (asema->_lauta[tempX][tempY] != NULL)
+		{
+			Nappula* ruudunNappula = asema->_lauta[tempX][tempY];
+
+			//Jos sijainnissa oleva nappula on saman v‰rinen niin sinne ei voi siirty‰
+			if (ruudunNappula->getVari() == asema->getSiirtovuoro())
+				continue;
+			else //Se lis‰t‰‰n listaan mahdollisista liikkeist‰ jos nappula on eri v‰rinen
+			{
+				Ruutu loppuRuutu = Ruutu(tempX, tempY);
+				Siirto siirto(*ruutu, loppuRuutu);
+				lista.push_back(siirto);
+			}
+		}
+		else
+		{
+			Ruutu loppuRuutu = Ruutu(tempX, tempY);
+				Siirto siirto(*ruutu, loppuRuutu);
+				lista.push_back(siirto);
+		}
+	}
 }
 
 
