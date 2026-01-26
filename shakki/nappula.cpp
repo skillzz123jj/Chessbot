60283a7cc468c09f891479fcdae225ab28060760
Mila <turpeinenmila6@gmail.com>
2026-01-26 02:34:04 +0200

    Add movement logic for King

--- shakki/nappula.cpp
+++ shakki/nappula.cpp
@@ -78,13 +78,47 @@ void Daami::annaSiirrot(std::list<Siirto>& lista, Ruutu* ruutu, Asema* asema, in
 
 void Kuningas::annaSiirrot(std::list<Siirto>& lista, Ruutu* ruutu, Asema* asema, int vari)
 {
-	/*perusidea on ett‰ kaikki viereiset ruudut ovat sallittuja. kuten tornilla ja l‰hetill‰,
-	oman nappulan p‰‰lle ei voi menn‰ ja vastustajan nappulan voi syˆd‰.
+	int x = ruutu->getRivi();
+	int y = ruutu->getSarake();
 
-	Kaikki muu kuninkaaseen liittyv‰ tarkistus tehd‰‰n eri paikassa*/
+	int siirrot[8][2] = {
+	   { -1, 1}, { 0, 1},
+	   {1,  1}, {-1, 0},
+	   { -1, -1}, { 0, -1},
+	   {1, -1}, { 1, 0 }
+	};
 
 
-	
+	for (int i = 0; i < 8; ++i)
+	{
+		int tempX = x + siirrot[i][0];
+		int tempY = y + siirrot[i][1];
+
+		//Katsotaan meneekˆ laudasta ulos
+		if (tempX < 0 || tempX >= 8 || tempY < 0 || tempY >= 8)
+			continue;
+
+		if (asema->_lauta[tempX][tempY] != NULL)
+		{
+			Nappula* ruudunNappula = asema->_lauta[tempX][tempY];
+
+			//Jos sijainnissa oleva nappula on saman v‰rinen niin sinne ei voi siirty‰
+			if (ruudunNappula->getVari() == asema->getSiirtovuoro())
+				continue;
+			else //Se lis‰t‰‰n listaan mahdollisista liikkeist‰ jos nappula on eri v‰rinen
+			{
+				Ruutu loppuRuutu = Ruutu(tempX, tempY);
+				Siirto siirto(*ruutu, loppuRuutu);
+				lista.push_back(siirto);
+			}
+		}
+		else
+		{
+			Ruutu loppuRuutu = Ruutu(tempX, tempY);
+			Siirto siirto(*ruutu, loppuRuutu);
+			lista.push_back(siirto);
+		}
+	}
 }
 
 
@@ -97,3 +131,5 @@ void Sotilas::annaSiirrot(std::list<Siirto>& lista, Ruutu* ruutu, Asema* asema, 
 void Sotilas::lisaaSotilaanKorotukset(Siirto* siirto, std::list<Siirto>& lista, Asema* asema) {
 	
 }
+
+
