f3882f08869795a885257c7b26ae3e9598c2f084
Anton Iljina <anton.iljina@outlook.com>
2026-03-02 12:23:26 +0200

    Fix incorrect if checks in black pawn eating

--- shakki/nappula.cpp
+++ shakki/nappula.cpp
@@ -748,7 +748,7 @@ void Sotilas::annaSiirrot(std::list<Siirto>& lista, Ruutu* ruutu, Asema* asema, 
 				}
 			}
 
-			if (x - 1 >= 0 && y - 1 <= 0)
+			if (x - 1 >= 0 && y - 1 >= 0)
 			{
 				//Tarkistaa saako vasemmalta syötyä vihollisen nappulan
 				if (asema->_lauta[y - 1][x - 1] != NULL
@@ -769,7 +769,7 @@ void Sotilas::annaSiirrot(std::list<Siirto>& lista, Ruutu* ruutu, Asema* asema, 
 				}
 			}
 
-			if (x - 1 >= 0 && y - 1 <= 8)
+			if (x - 1 >= 0 && y - 1 >= 0)
 			{
 				//Tarkistaa saako oikealta syötyä vihollisen nappulan
 				if (asema->_lauta[y - 1][x + 1] != NULL
